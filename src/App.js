385ed56df2a4d0cd5e20a3f9f0cd029984faecd6
Nick <Nick>
2021-04-19 15:45:49 +0530

    Initial Material Todo Component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ToDoMaterial from './ToDo-Material/TodoMaterial';
+
+const App = () =>
+{
+  return(
+    <>
+            <ToDoMaterial />
+    </>
+  );
+}
+
+export default App;
